c2690af094b4b8b1474543a40354a5c8169e795c
0chen <admin@hi-ourlife.com>
2022-01-21 02:18:31 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 from datetime import *
-from dateutil.tz import *
+import pytz
 SECRET = os.getenv('SECRET')
 WEBHOOK = os.getenv('WEBHOOK')
 
@@ -9,24 +9,24 @@ bUPIds = os.getenv('BUPIDS').split(',')if os.getenv('BUPIDS') is not None else '
 bAIds = os.getenv('BAIDS').split(',')if os.getenv('BAIDS') is not None else ''
 aAIds = os.getenv('AUPIDS').split(',')if os.getenv('AUPIDS') is not None else ''
 
-
+shanghai = pytz.timezone('Asia/Shanghai')
 breakfast = os.getenv('BREAKFAST')
 if breakfast is not None and breakfast != '':
     breakfast = (
         datetime.strptime(breakfast, '%H') if len(breakfast) == 2 else datetime.strptime(
             breakfast, '%H%M'))
-    breakfast.astimezone(tz.gettz('Asia/Shanghai'))
+    breakfast.astimezone(shanghai)
 
 lunch = os.getenv('LUNCH')
 if lunch is not None and lunch != '':
     lunch = (
         datetime.strptime(lunch, '%H') if len(lunch) == 2 else datetime.strptime(
             lunch, '%H%M'))
-    lunch.astimezone(tz.gettz('Asia/Shanghai'))
+    lunch.astimezone(shanghai)
 
 dinner = os.getenv('DINNER')
 if dinner is not None and dinner != '':
     dinner = (
         datetime.strptime(dinner, '%H') if len(dinner) == 2 else datetime.strptime(
             dinner, '%H%M'))
-    dinner.astimezone(tz.gettz('Asia/Shanghai'))
+    dinner.astimezone(shanghai)
